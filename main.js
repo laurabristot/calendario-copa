c3d7b82311314f9aeb8b5657c02c5b90fc84660f
Laura Bristot <107277969+laurabristot@users.noreply.github.com>
2022-11-04 19:50:37 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+const logo = document.querySelector('#logo')
+
+const bodyBlue = document.querySelector('.blue')
+bodyBlue.addEventListener("click", function () {
+  bodyBlue.classList.remove('blue')
+  bodyBlue.classList.add('yellow')
+  
+  
+  const bodyYellow = document.querySelector('.yellow')
+  bodyYellow.addEventListener("click", function () {
+ 
+    bodyYellow.classList.remove('yellow')
+    bodyYellow.classList.add('green')
+    
+    const bodyGreen = document.querySelector('.green')
+    bodyGreen.addEventListener("click", function () {  
+      bodyGreen.classList.remove('green')
+      bodyGreen.classList.add('blue')
+}) 
+})
+})
+
+
+
+
+
+
+
+function createGame(player1, hour, player2) {
+  return `
+  <li>
+    <img src="./assets/icon-${player1}.svg" alt="ícone ${player1}" />
+    <strong>${hour}</strong>
+    <img src="./assets/icon-${player2}.svg" alt="ícone ${player1}" />
+  </li>
+  `
+}
+
+let delay = -0.3
+
+function createCard(date, day, games) {
+  delay = delay + 0.3
+  return `  
+  <div class="card" style="animation-delay: ${delay}s">
+    <h2>${date}<span>${day}</span></h2>
+     <ul>
+     ${games}
+    </ul>
+  </div>`
+}
+
+document.querySelector('#cards').innerHTML =
+  createCard('24/11', 'quinta', createGame('brazil', '16:00', 'serbia')) +
+  createCard(
+    '28/11',
+    'segunda',
+    createGame('brazil', '13:00', 'switzerland') +
+      createGame('portugal', '16:00', 'uruguay')
+  ) +
+  createCard('02/12', 'sexta', createGame('brazil', '16:00', 'cameroon'))
